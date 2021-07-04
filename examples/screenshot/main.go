4f328f963ea7aa00a8b0ce0b97d8019f98d60ef5
zhongkaizhu <zhongkaizhu@tencent.com>
2021-07-04 20:44:43 +0800

    update example/screenshot with adjusting quality

--- examples/screenshot/main.go
+++ examples/screenshot/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/electricbubble/gwda"
-	"image"
+	"io/ioutil"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
@@ -12,30 +14,62 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	screenshot, err := driver.Screenshot()
-	if err != nil {
-		log.Fatal(err)
+	capture := func(name string) (int, error) {
+		screenshot, err := driver.Screenshot()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buf, _ := ioutil.ReadAll(screenshot)
+		err = os.WriteFile(name, buf, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return len(buf), err
 	}
 
+	settings, err := driver.GetAppiumSettings()
+	log.Printf("get settings: %#v", settings)
+
+	// set wda screenshot quality to high
+	_, err = driver.SetAppiumSettings(map[string]interface{}{
+		"screenshotQuality":1,
+	})
+	t1 := time.Now()
+	length, err := capture("full_quality.png")
+	elapsed := time.Since(t1)
+	log.Printf("full quality time:%v, size:%v", elapsed, length)
+
+	// set wda screenshot quality to low
+	_, err = driver.SetAppiumSettings(map[string]interface{}{
+		"screenshotQuality":2,
+	})
+	t1 = time.Now()
+	length, err = capture("low_quality.png")
+	elapsed = time.Since(t1)
+	log.Printf("low quality time:%v, size:%v", elapsed, length)
+
+	/*
 	img, format, err := image.Decode(screenshot)
 	if err != nil {
 		log.Fatal(err)
 	}
 	_, _ = img, format
-	// userHomeDir, _ := os.UserHomeDir()
-	// file, err := os.Create(userHomeDir + "/Desktop/s1." + format)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer func() { _ = file.Close() }()
-	// switch format {
-	// case "png":
-	// 	err = png.Encode(file, img)
-	// case "jpeg":
-	// 	err = jpeg.Encode(file, img, nil)
-	// }
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Println(file.Name())
+	userHomeDir, _ := os.UserHomeDir()
+	file, err := os.Create(userHomeDir + "/Desktop/s1." + format)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() { _ = file.Close() }()
+	switch format {
+	case "png":
+		err = png.Encode(file, img)
+	case "jpeg":
+		err = jpeg.Encode(file, img, nil)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(file.Name())
+	 */
 }
